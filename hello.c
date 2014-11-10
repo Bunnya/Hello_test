079fa7a74965018e820df9c69cc9dd93083472dd
RyomaABE <arayahomuhomu@gmail.com>
2014-11-10 21:00:05 +0900

    a+b

--- hello.c
+++ hello.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 int main(){
+    int a, b;
 	puts("Hello");
 	puts("aaa");
               puts("bbb");
-	return 0;
+    a = 1;
+    b = 3;
+    printf("%d", a+b);	
+    return 0;
 }
